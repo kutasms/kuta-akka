54f4c600e3ac813449fcdd0eedc0c77c3cb6d188
kutasms <7437280@qq.com>
2020-09-02 05:15:02 +0800

    新增注释

--- base/src/main/java/com/kuta/base/annotation/PrimaryKey.java
+++ base/src/main/java/com/kuta/base/annotation/PrimaryKey.java
@@ -6,6 +6,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+
+/**
+ * 主键标识注解
+ * <p>在数据实体的数据字段添加本注解将可以使用KutaSQLUtil.isPrimaryKey判断是否为主键</p>
+ * */
 @Documented
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
