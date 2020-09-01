af39f114a21a6b3d80cdbc2134cb4135c7db14b5
kutasms <7437280@qq.com>
2020-09-02 05:01:41 +0800

    新增主键标识注解插件，此注解应用于数据实体字段，在mybatis生成器的配置文件中增加对此注解插件的应用。

---
+++ base/src/main/java/com/kuta/base/mybatis/PrimaryKeyPlugin.java
@@ -0,0 +1,42 @@
+package com.kuta.base.mybatis;
+
+import java.util.List;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+
+import com.kuta.base.annotation.PrimaryKey;
+
+public class PrimaryKeyPlugin extends org.mybatis.generator.api.PluginAdapter {
+
+	private FullyQualifiedJavaType primaryKey;
+	
+	@Override
+	public boolean validate(List<String> warnings) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	
+	public PrimaryKeyPlugin() {
+		super();
+		primaryKey = new FullyQualifiedJavaType(PrimaryKey.class.getName());
+	}
+	
+	@Override
+	public boolean modelFieldGenerated(Field field, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn,
+			IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+		// TODO Auto-generated method stub
+//		
+		if(introspectedTable.getPrimaryKeyColumns().contains(introspectedColumn)){
+			topLevelClass.addImportedType(primaryKey);
+			field.addAnnotation("@PrimaryKey");
+		}
+		return true;
+//		return super.modelFieldGenerated(field, topLevelClass, introspectedColumn, introspectedTable, modelClassType);
+	}
+
+
+}
