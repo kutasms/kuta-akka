7df950759225e7489884752dcf32506156e7063c
kutasms <7437280@qq.com>
2020-10-29 07:14:41 +0800

    版本更新，使其能再内网任意机器执行管线操作，并且能与redis集群融合

--- base/src/main/java/com/kuta/base/database/mysql/MysqlToRedisPipeUtil.java
+++ base/src/main/java/com/kuta/base/database/mysql/MysqlToRedisPipeUtil.java
@@ -5,14 +5,20 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
 
 import com.kuta.common.config.utils.PropertyUtil;
+
+import redis.clients.jedis.HostAndPort;
 
 /**
  * 以linux管线将mysql数据导入redis的工具类
@@ -25,7 +31,7 @@ public class MysqlToRedisPipeUtil {
 	 * @param desc 说明信息
 	 * @return 执行结果
 	 * */
-	public static boolean execute(String sqlFileName, String desc) {
+	public static boolean execute(String sqlFileName, String desc, HostAndPort hostAndPort) {
 		URL url = MysqlToRedisPipeUtil.class.getClassLoader().getResource(sqlFileName);
 		String sqlPath = url.getPath();
 
@@ -35,8 +41,14 @@ public class MysqlToRedisPipeUtil {
 		command.add("-c");
 		arguments.add("mysql");
 		arguments.add("-uroot");
-		arguments.add("-p" + PropertyUtil.getProperty("jdbc", "jdbc.password"));
-		arguments.add("-DtbSimCity");
+		String password = PropertyUtil.getProperty("jdbc", "jdbc.password");
+		if(password.contains("&")) {
+			password = password.replace("&", "\\&");
+		}
+		arguments.add("-p" + password);
+		arguments.add("-P" + PropertyUtil.getProperty("jdbc", "jdbc.port"));
+		arguments.add("-Dsimcity");
+		arguments.add("-h" + PropertyUtil.getProperty("jdbc", "jdbc.host"));
 		arguments.add("--default-character-set=utf8");
 		// commandList.add("-h");
 		// commandList.add(PropertyUtils.getProperty("jdbc", "jdbc.host"));
@@ -46,12 +58,16 @@ public class MysqlToRedisPipeUtil {
 		arguments.add(sqlPath);
 		arguments.add("|");
 		arguments.add("redis-cli");
+		arguments.add("-h");
+		arguments.add(hostAndPort.getHost());
 		arguments.add("-p");
-		arguments.add(PropertyUtil.getProperty("redis", "redis.port"));
+		arguments.add(String.valueOf(hostAndPort.getPort()));
 		arguments.add("-a");
 		arguments.add(PropertyUtil.getProperty("redis", "redis.pwd"));
+		arguments.add("-c");
 		arguments.add("--pipe");
 		command.add(StringUtils.join(arguments, " "));
+		System.out.println(Arrays.deepToString(arguments.toArray()));
 		ProcessBuilder pb = new ProcessBuilder(command);
 
 		File file = new File(url.getPath());
