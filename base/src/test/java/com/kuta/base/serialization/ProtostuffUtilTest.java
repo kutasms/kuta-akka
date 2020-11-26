722aff45ee16486ba6ea078a36448cf5bcbd1748
kutasms <7437280@qq.com>
2020-11-26 18:40:26 +0800

    对相关模块进行整合

--- base/src/test/java/com/kuta/base/serialization/ProtostuffUtilTest.java
+++ base/src/test/java/com/kuta/base/serialization/ProtostuffUtilTest.java
@@ -7,26 +7,13 @@ import java.util.List;
 import java.util.Map;
 
 import com.kuta.base.util.KutaConsoleUtil;
+import com.kuta.base.util.KutaUtil;
 
 import junit.framework.TestCase;
 
 public class ProtostuffUtilTest extends TestCase {
 	
 	public void testMapOrList(){
-		Map<String, Object> map = new HashMap<>();
-        map.put("key1", "butioy");
-        map.put("key2", "protostuff");
-        map.put("key3", "serialize");
-        
-        List<String> list = new ArrayList<>();
-        list.add("fiallfewafew");
-        list.add("biialzzz");
-//        SerializeDeserializeWrapper wrapper = SerializeDeserializeWrapper.builder(list);
-
-        byte[] serializeBytes = ProtostuffUtil.serialize(map);
-        System.out.println("序列化map集合二进制数组长度 length=" + serializeBytes.length);
-        System.out.println("bytes:" + Arrays.toString(serializeBytes));
-        ProtostuffObjectWrapper deserializeWrapper = ProtostuffUtil.deserialize(serializeBytes, ProtostuffObjectWrapper.class);
-        System.out.println("反序列化map对象：" + deserializeWrapper.getData());
+		System.out.println(String.format("playersummary_%s", KutaUtil.intToBase64(50369)));
 	}
 }
