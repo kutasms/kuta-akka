16d03d2a3aac966ed7dc4f83b782778a79a19e26
王智 <7437280@qq.com>
2020-12-27 22:02:07 +0800

    加入响应编码

---
+++ akka-base/src/main/java/com/kuta/akka/base/entity/ResponseStatus.java
@@ -0,0 +1,13 @@
+package com.kuta.akka.base.entity;
+
+public class ResponseStatus {
+	/**
+	 * 完成状态
+	 * */
+	public final static int OK = 0;
+	
+	/**
+	 * 未知错误
+	 * */
+	public final static int UNKNOWN_ERROR = -1;
+}
