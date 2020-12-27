3dab28750fa8f322edbf5fc88378902d70793c57
王智 <7437280@qq.com>
2020-12-27 22:00:28 +0800

    新增websocket基础服务类，包括参数检测、发送成功和失败消息

---
+++ akka-base/src/main/java/com/kuta/akka/base/WebsocketServiceActor.java
@@ -0,0 +1,59 @@
+package com.kuta.akka.base;
+
+import com.alibaba.fastjson.JSONObject;
+import com.kuta.akka.base.entity.GatewayMessage;
+import com.kuta.akka.base.entity.ResponseStatus;
+import com.kuta.akka.base.entity.WebSocketResponse;
+import com.kuta.base.exception.KutaAsserts;
+import com.kuta.base.exception.KutaRuntimeException;
+import com.kuta.base.util.ThrowingConsumer;
+
+import akka.actor.ActorRef;
+
+public abstract class WebsocketServiceActor extends KutaActor {
+
+	protected void inspect(GatewayMessage msg, 
+			ThrowingConsumer<WebSocketResponse, Exception> consumer,
+			String... args) {
+		WebSocketResponse response = new WebSocketResponse();
+		response.setCode(msg.getCode());
+		try {
+			if (args != null && args.length > 0) {
+				for (String arg : args)
+					KutaAsserts.notNull(arg, arg);
+			}
+		} catch (Exception ex) {
+			logger.error(ex,ex.getMessage());
+			response.setMessage(ex.getMessage());
+			response.setStatus(ResponseStatus.UNKNOWN_ERROR);
+			msg.getChannel().tell(response.toJSONString(), self());
+		}
+	}
+	
+	protected void success(ActorRef channel, WebSocketResponse rsp, JSONObject data) {
+		if(!channel.isTerminated()) {
+			rsp.setStatus(ResponseStatus.OK);
+			rsp.setMessage("OK");
+			if(data != null) {
+				rsp.setData(data);
+			}
+			channel.tell(rsp.toJSONString(), self());
+		} else {
+			throw new KutaRuntimeException("websocket通道已关闭");
+		}
+	}
+	
+	protected void failure(ActorRef channel, 
+			WebSocketResponse rsp, 
+			String errorMsg, 
+			int errorCode) {
+		if(!channel.isTerminated()) {
+			rsp.setMessage(errorMsg);
+			rsp.setStatus(errorCode);
+			channel.tell(rsp.toJSONString(), self());
+		} else {
+			throw new KutaRuntimeException("websocket通道已关闭");
+		}
+	}
+
+}
