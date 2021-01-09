308470e5b6bfb8a0107269b1374f6c5942535f6a
王智 <7437280@qq.com>
2021-01-10 00:29:14 +0800

    增加响应状态码的基础类

---
+++ base/src/main/java/com/kuta/base/communication/ResponseStatus.java
@@ -0,0 +1,26 @@
+package com.kuta.base.communication;
+
+public class ResponseStatus {
+	/**
+	 * 	完成状态
+	 * */
+	public final static int OK = 0;
+	
+	/**
+	 * 	未知错误
+	 * */
+	public final static int UNKNOWN_ERROR = -1;
+	/**
+	 * 	未找到合适的actor处理消息
+	 * */
+	public final static int NON_MATCHED_ACTOR = -2;
+	/**
+	 * 	无法解析的指令
+	 * */
+	public final static int UNKNOWN_CODE = -3;
+	
+	/**
+	 *	参数丢失
+	 * */
+	public final static int ERROR_ARGUMENT_LOSE = -4;
+}
