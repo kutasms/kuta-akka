7fa8ee95e71a59ed72f13516d729e5cb338777b9
王智 <7437280@qq.com>
2021-01-24 21:35:05 +0800

    增加设备编号、站点编号

--- akka-base/src/main/java/com/kuta/akka/base/entity/GatewayBinaryMessage.java
+++ akka-base/src/main/java/com/kuta/akka/base/entity/GatewayBinaryMessage.java
@@ -4,8 +4,9 @@ package com.kuta.akka.base.entity;
  * 网关二进制消息
  * */
 public class GatewayBinaryMessage extends GatewayMessage {
-	private int uid;
+	private int sid;
 	private byte[] data;
+	private int did;
 	
 	/**
 	 * 获取数据
@@ -22,13 +23,19 @@ public class GatewayBinaryMessage extends GatewayMessage {
 	/**
 	 * 获取用户编号
 	 * */
-	public int getUid() {
-		return uid;
+	public int getSid() {
+		return sid;
 	}
 	/**
 	 * 设置用户编号
 	 * */
-	public void setUid(int uid) {
-		this.uid = uid;
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+	public int getDid() {
+		return did;
+	}
+	public void setDid(int did) {
+		this.did = did;
 	}
 }
