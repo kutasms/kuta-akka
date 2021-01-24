697876bb16152936e1ed8f51357f7049c5517edc
王智 <7437280@qq.com>
2021-01-24 21:33:07 +0800

    名称重构

---
+++ akka-base/src/main/java/com/kuta/akka/base/entity/KutaWebSocketResponse.java
@@ -0,0 +1,21 @@
+package com.kuta.akka.base.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.annotation.JSONField;
+import com.alibaba.fastjson.serializer.SimplePropertyPreFilter;
+
+/**
+ * WebSocket回复消息
+ * */
+public class KutaWebSocketResponse extends KutaResponse implements Serializable {
+	
+	/**
+	 * 序列化编号
+	 */
+	private static final long serialVersionUID = -4106145497077300790L;
+	
+	
+}
