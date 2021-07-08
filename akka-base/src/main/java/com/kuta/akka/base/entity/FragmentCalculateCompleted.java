cc090c58ad99028802e2d7bf03801d28bcb716e4
王智 <7437280@qq.com>
2021-07-08 20:53:07 +0800

    数据片段计算完毕事件

---
+++ akka-base/src/main/java/com/kuta/akka/base/entity/FragmentCalculateCompleted.java
@@ -0,0 +1,10 @@
+package com.kuta.akka.base.entity;
+
+import com.kuta.akka.base.serialization.KutaSerialMessage;
+
+/**
+ * 	数据片段计算完毕
+ * */
+public class FragmentCalculateCompleted extends KutaSerialMessage{
+
+}
