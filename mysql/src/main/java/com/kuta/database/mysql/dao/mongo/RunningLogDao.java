c612d0a79c9a0cf52a9783a62e9a3ec28003825e
王智 <7437280@qq.com>
2021-07-09 21:00:52 +0800

    系统运行日志Dao

---
+++ mysql/src/main/java/com/kuta/database/mysql/dao/mongo/RunningLogDao.java
@@ -0,0 +1,11 @@
+package com.kuta.database.mysql.dao.mongo;
+
+import com.kuta.base.database.mongodb.KutaMongoDao;
+import com.kuta.database.mysql.pojo.mongo.RunningLog;
+
+public class RunningLogDao extends KutaMongoDao<RunningLog> {
+
+	public static RunningLogDao get(RunningLog log) {
+		return new RunningLogDao();
+	}
+}
