5e152203a3d3ab5dd7ad8cd7462f39a91c69fd0d
王智 <7437280@qq.com>
2021-07-20 11:49:38 +0800

    自动生成SqlSession和JedisClient工具

---
+++ base/src/main/java/com/kuta/base/util/KutaDataUtil.java
@@ -0,0 +1,27 @@
+package com.kuta.base.util;
+
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.kuta.base.cache.JedisClient;
+import com.kuta.base.database.KutaSQLUtil;
+
+public class KutaDataUtil {
+	public static void execute(BiConsumer<SqlSession,JedisClient> consumer) throws Exception {
+		KutaSQLUtil.exec(session->{
+			KutaRedisUtil.exec(jedis->{
+				consumer.accept(session, jedis);
+			});
+		});
+	}
+	public static <T> T func(BiFunction<SqlSession,JedisClient, T> func) throws Exception {
+		return KutaSQLUtil.func(session->{
+			return KutaRedisUtil.func(jedis->{
+				return func.apply(session, jedis);
+			});
+		});
+
+	}
+}
