b430a7f453565e4d8162ca061598cb48bb03b326
王智 <7437280@qq.com>
2021-07-20 11:40:39 +0800

    将mysql库正式改名为data库，增加bigdecimal, biginteger的json反序列化集成

--- base/src/main/java/com/kuta/base/json/BigDecimalDeserializer.java
+++ base/src/main/java/com/kuta/base/json/BigDecimalDeserializer.java
@@ -1,16 +1,18 @@
 package com.kuta.base.json;
 
 import java.lang.reflect.Type;
+import java.math.BigDecimal;
 
 import com.alibaba.fastjson.parser.DefaultJSONParser;
 import com.alibaba.fastjson.parser.deserializer.ObjectDeserializer;
 
-public class BigInteger2JSONDeserializer implements ObjectDeserializer{
+public class BigDecimalDeserializer implements ObjectDeserializer {
 
 	@Override
-	public <T> T deserialze(DefaultJSONParser parser, Type type, Object fieldName) {
+	public BigDecimal deserialze(DefaultJSONParser parser, Type type, Object fieldName) {
 		// TODO Auto-generated method stub
-		return (T) parser.lexer.stringVal();
+		
+		return new BigDecimal(parser.getLexer().stringVal());
 	}
 
 	@Override
--- base/src/main/java/com/kuta/base/json/BigIntegerDeserializer.java
+++ base/src/main/java/com/kuta/base/json/BigIntegerDeserializer.java
@@ -1,16 +1,17 @@
 package com.kuta.base.json;
 
 import java.lang.reflect.Type;
+import java.math.BigInteger;
 
 import com.alibaba.fastjson.parser.DefaultJSONParser;
 import com.alibaba.fastjson.parser.deserializer.ObjectDeserializer;
 
-public class BigDecimal2JSONDeserializer implements ObjectDeserializer {
+public class BigIntegerDeserializer implements ObjectDeserializer{
 
 	@Override
-	public <T> T deserialze(DefaultJSONParser parser, Type type, Object fieldName) {
+	public BigInteger deserialze(DefaultJSONParser parser, Type type, Object fieldName) {
 		// TODO Auto-generated method stub
-		return (T) parser.getLexer().stringVal();
+		return new BigInteger(parser.lexer.stringVal());
 	}
 
 	@Override
