10654222afba7ae0413276c49c021fabd9507b4d
kutasms <7437280@qq.com>
2021-08-10 13:45:51 +0800

    逻辑优化

--- akka-base/src/main/java/com/kuta/akka/base/auto/PluginScheduler.java
+++ akka-base/src/main/java/com/kuta/akka/base/auto/PluginScheduler.java
@@ -1,17 +1,8 @@
 package com.kuta.akka.base.auto;
 
 import org.quartz.CronScheduleBuilder;
-import org.quartz.CronTrigger;
-import org.quartz.Job;
-import org.quartz.JobDetail;
-import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
-import org.quartz.SimpleScheduleBuilder;
-import org.quartz.Trigger;
 import org.quartz.TriggerBuilder;
-import org.quartz.impl.StdScheduler;
-import org.quartz.impl.StdSchedulerFactory;
-import org.quartz.impl.triggers.CronTriggerImpl;
 
 import com.typesafe.akka.extension.quartz.QuartzSchedulerExtension;
 
@@ -29,17 +20,17 @@ public class PluginScheduler {
 		
 	}
 	
-	private QuartzSchedulerExtension extension = null;
-	private PluginDataSource dataSource = null;
+//	private QuartzSchedulerExtension extension = null;
+//	private PluginDataSource dataSource = null;
 	
 	
 	
 	public PluginScheduler withExtension(QuartzSchedulerExtension extension) throws SchedulerException {
-		this.extension = extension;
+//		this.extension = extension;
 		TriggerBuilder.newTrigger().withSchedule(CronScheduleBuilder.cronSchedule("")).build();
-		CronTrigger cronTrigger;
-		JobDetail detail;
-		Scheduler defaultScheduler = StdSchedulerFactory.getDefaultScheduler();
+//		CronTrigger cronTrigger;
+//		JobDetail detail;
+//		Scheduler defaultScheduler = StdSchedulerFactory.getDefaultScheduler();
 		//		this.extension.scheduler().
 		PluginDataSource<JSONTaskData> data = null;
 		PluginJobBuilder
