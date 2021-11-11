8c23ddd17f210ec0a42fb8d343f522c4a05b19a2
kutasms <7437280@qq.com>
2021-11-11 18:13:59 +0800

    add count property in this message.

--- akka-base/src/main/java/com/kuta/akka/base/entity/FragmentCalculateCompleted.java
+++ akka-base/src/main/java/com/kuta/akka/base/entity/FragmentCalculateCompleted.java
@@ -6,5 +6,13 @@ import com.kuta.akka.base.serialization.KutaSerialMessage;
  * 	数据片段计算完毕
  * */
 public class FragmentCalculateCompleted extends KutaSerialMessage{
+	private int count = 0;
 
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
 }
