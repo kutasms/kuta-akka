f6cd794f42bca29cab396b796560afedb4a9fb31
kutasms <7437280@qq.com>
2021-11-19 13:11:03 +0800

    fixed a bug that the message is not found on centos server.

--- common-config/src/main/java/com/kuta/common/config/utils/I18nUtil.java
+++ common-config/src/main/java/com/kuta/common/config/utils/I18nUtil.java
@@ -1,20 +1,23 @@
 package com.kuta.common.config.utils;
 
 import java.util.Locale;
-import java.util.Map;
+import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class I18nUtil {
 	
 	private static boolean initialized = false;
 	private static Locale locale = null;
-	private static Map<String, ResourceBundle> map;
 	
 	public static void initialize(String locale) {
 		if(!initialized) {
-			I18nUtil.locale = new Locale(locale);
-			map = new ConcurrentHashMap<String, ResourceBundle>();
+			if(locale.contains("-")) {
+				String[] ls = locale.split("-");
+				I18nUtil.locale = new Locale(ls[0], ls[1]);
+			}
+			else {
+				I18nUtil.locale = new Locale(locale);
+			}
 		}
 		initialized = true;
 	}
@@ -23,13 +26,11 @@ public class I18nUtil {
 		if(!initialized) {
 			throw new RuntimeException("please initialize the I18nUtil class.");
 		}
-		if(map.containsKey(prefix)) {
-			ResourceBundle bundle = map.get(prefix);
-			return bundle.getString(key);
-		} else {
-			ResourceBundle rb = ResourceBundle.getBundle(String.format("locale/%s", prefix), locale);
-			map.put(prefix, rb);
-			return rb.getString(key);
-		}
+//		String currDir = System.getProperty("user.dir");
+//		String sourceBaseName = String.format("%s/app-config/locale/%s",currDir, prefix);
+		String sourceBaseName = String.format("locale/%s", prefix);
+//		System.out.println(sourceBaseName);
+		ResourceBundle rb = ResourceBundle.getBundle(sourceBaseName, locale, I18nUtil.class.getClassLoader());
+		return rb.getString(key);
 	}
 }
