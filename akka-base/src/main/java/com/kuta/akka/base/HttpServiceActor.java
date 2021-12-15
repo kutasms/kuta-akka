2cb7ea081b138f027a5062c4a04a1a8727987a2a
王智 <7437280@qq.com>
2021-12-15 14:20:12 +0800

    The basic processing logic of optimization is as follows: 1. Automatically roll back when an error occurs in updating data 2. If automatic reprocess is required, resend the message to self actor.

--- akka-base/src/main/java/com/kuta/akka/base/HttpServiceActor.java
+++ akka-base/src/main/java/com/kuta/akka/base/HttpServiceActor.java
@@ -89,7 +89,7 @@ public abstract class HttpServiceActor extends KutaActor {
 		KutaHttpResponse httpRsp = null;
 		KutaWebSocketResponse websocketRsp = null;
 		boolean isWebsocket = msg.getProtocol().equals(KutaConstants.PROTOCAL_WEBSOCKET);
-		DataSessionFactory dataSessionFactory = DataSessionFactory.create();
+		DataSessionFactory f = DataSessionFactory.create();
 		if(isWebsocket) {
 			websocketRsp = new KutaWebSocketResponse();
 			websocketRsp.setCode(msg.getCode());
@@ -109,14 +109,15 @@ public abstract class HttpServiceActor extends KutaActor {
 				}
 			}
 			if(isWebsocket) {
-				rsp.accept(websocketRsp,dataSessionFactory);
+				rsp.accept(websocketRsp,f);
 			} else {
-				rsp.accept(httpRsp,dataSessionFactory);
+				rsp.accept(httpRsp,f);
 			}
 			
 		} 
 		catch(KutaIllegalArgumentException ex) {
 			logger.error(ex,ex.getMessage());
+			f.rollback();
 			if(isWebsocket) {
 				websocketRsp.setMessage(ex.getMessage());
 				websocketRsp.setStatus(ex.getErrorCode());
@@ -131,6 +132,7 @@ public abstract class HttpServiceActor extends KutaActor {
 		}
 		catch (Exception ex) {
 			logger.error(ex,ex.getMessage());
+			f.rollback();
 			if(isWebsocket) {
 				websocketRsp.setMessage(ex.getMessage());
 				websocketRsp.setStatus(ResponseStatus.UNKNOWN_ERROR);
@@ -145,7 +147,11 @@ public abstract class HttpServiceActor extends KutaActor {
 				msg.getChannel().tell(httpRsp, self());
 			}
 		} finally {
-			dataSessionFactory.release();
+			f.release();
+			if(f.isReprocessOnErrorOccurred() 
+					&& f.isRollbacked()) {
+				self().tell(msg, sender());
+			}
 		}
 	}
 	
